58ccdf20a98c2ebfaa2a5e7e4b2e2ec19479dca2
JCPL-CHXD <jcplchxd@gmail.com>
2026-09-25 23:35:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='uix-node2d-v13';
+const CACHE_NAME='uix-node2d-v18';
 const ROOT=new URL('./',self.location.href);
 
 function sameOrigin(url){return url.origin===self.location.origin;}
