a73ad98a8440fbb437ced9a23e9e9b0709326723
JCPL-CHXD <jcplchxd@gmail.com>
2026-09-26 19:46:36 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='uix-node2d-vfix54';
+const CACHE_NAME='uix-node2d-vfix61-merged-runtime';
 const ROOT=new URL('./',self.location.href);
 const EXTRA_FILES=['help/index.html','help/help.json','updates/index.html','updates/changes.json'];
 
